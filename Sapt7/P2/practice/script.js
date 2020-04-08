82a35d9cc823f7c51e2901a6f6b06940fb709334
antoniaprof <antonia.profiroiu@gmail.com>
2020-04-08 20:35:42 +0300

    Modified Sapt7/ P2/ practice

--- Sapt7/P2/practice/script.js
+++ Sapt7/P2/practice/script.js
@@ -1,54 +1,51 @@
 function protectEmail(email) {
 
-    console.log(email.slice(0, 4) + ' ... ' + email.slice(-10));
+  console.log(email.slice(0, 4) + ' ... ' + email.slice(-10));
 }
 console.log(protectEmail('antonia.profi@yahoo.com'));
 
-// function capitalize (value) {
-//     var word = value.split(' ');
-//     var letter = value.charAt(0)
-//     console.log(word);
-//     console.log( letter.toUpperCase());
-//     for (i=0; i<word.length; i++) {
-//       word[i] = word[i].toUpperCase + word[i].slice(1);
-//     }
-
-//   };
-//   capitalize('i am superman');
+function capitalize(value) {
+  var array = value.split(' ');
+  for (i = 0; i < array.length; i++) {
+    var letter = array[i].charAt(0).toUpperCase();
+    array[i] = letter + array[i].slice(1);
+  }
+  return array.join(' ');
+};
+console.log(capitalize('i am superman'));
 
 function wordSwitch(value) {
-    var myArray = value.split('');
-    var i = 0;
-    while (i < myArray.length) {
-        if (myArray[i] === myArray[i].toUpperCase()) {
-            myArray[i] = myArray[i].toLowerCase();
-        } else {
-            myArray[i] = myArray[i].toUpperCase();
-        };
-        i++;
+  var myArray = value.split('');
+  var i = 0;
+  while (i < myArray.length) {
+    if (myArray[i] === myArray[i].toUpperCase()) {
+      myArray[i] = myArray[i].toLowerCase();
+    } else {
+      myArray[i] = myArray[i].toUpperCase();
     };
-    return myArray.join('');
+    i++;
+  };
+  return myArray.join('');
 
 };
 console.log(wordSwitch('DGGDGDssddDjdkdjkjlkfhsjhdDSDTYGUKGJKGJKHUGjghjghg'));
 
 function concatStrings(value, number) {
-    var string = '';
-   for (var i = 0; i < number; i++) {
-      string=string.concat(value);
-    }
-    return string;
+  var string = '';
+  for (var i = 0; i < number; i++) {
+    string = string.concat(value);
   }
-  
-  console.log(concatStrings('Wantsome', 6))
+  return string;
+}
 
-  function palindrome (value) {
-    var checkPalindrome = value.split('');
-    if (checkPalindrome.reverse().join('') === value) {
-      return value + ' is palindrome!';
-    } //else nu are sens
-      return value + " isn't a  palindrome"; 
-  };
-  console.log(palindrome('ana'));
+console.log(concatStrings('Wantsome', 6))
 
-  
+function palindrome(value) {
+  var checkPalindrome = value.split('');
+  if (checkPalindrome.reverse().join('') === value) {
+    return value + ' is palindrome!';
+  } //else nu are sens
+  return value + " isn't a  palindrome";
+};
+console.log(palindrome('ana'));
+
