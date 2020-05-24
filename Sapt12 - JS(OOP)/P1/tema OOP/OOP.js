4da1da7361f05343307049af00968483d72a6595
antoniaprof <antonia.profiroiu@gmail.com>
2020-05-24 19:52:10 +0300

    Modified Sapt12/P1/tema OOP

--- Sapt12 - JS(OOP)/P1/tema OOP/OOP.js
+++ Sapt12 - JS(OOP)/P1/tema OOP/OOP.js
@@ -3,25 +3,25 @@
 // triunghi.getPerimeter();              // 48
 // triunghi.getType();                   // "triunghi"
 
-function Triangle(a,b,c) {
+function Triangle(a, b, c) {
   this.a = a;
-  this.b= b;
-  this.c =c;
+  this.b = b;
+  this.c = c;
   this.type = 'triunghi';
-}
+};
 
 const forma = {
-getType: function() {
-  return this.type
-}
-}
-Triangle.prototype.forma = forma
+  getType: function () {
+    return this.type;
+  }
+};
+Triangle.prototype.forma = forma;
 Triangle.prototype.constructor = Triangle;
 
-let triunghi = new Triangle (6,6,6)
+let triunghi = new Triangle(6, 6, 6)
 triunghi.getPerimeter = function () {
- return this.a + this.b + this.c;
-}
+  return this.a + this.b + this.c;
+};
 console.log(triunghi.getPerimeter());
 console.log(triunghi.getType());
 console.log(forma.isPrototypeOf(triunghi));
